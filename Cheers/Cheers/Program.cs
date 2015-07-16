c8ab6bc584a1239ad00b8aa5f8bca8c9e51b1cfe
johnwquarles <john.w.quarles@gmail.com>
2015-07-16 14:10:02 -0500

    assignment done for Sat. Alhough user input isn't checked; if birthday is put in incorrectly, program messes up. Also get whitespace errors in git, probably need to fix some setting in Xamarin

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -11,11 +11,16 @@ namespace Cheers
 		{
 			Console.WriteLine ("Your name, please?");
 			string name = Console.ReadLine ();
+			Console.WriteLine ("And your birthday? MM/DD please!");
+			string DOB = Console.ReadLine ();
+
 			foreach (char c in name)
 			{
 				Console.WriteLine(Cheer (c));
 			}
 			Console.WriteLine(name.ToUpper() + " is.. GRAND!");
+			//Console.WriteLine ("\n");
+			Console.WriteLine(BDayLine (DOB));
 		}
 
 		private static string Cheer (char c)
@@ -23,6 +28,35 @@ namespace Cheers
 			string article;
 			article = (an_letters.Contains (Char.ToUpper(c)) || an_letters.Contains(Char.ToLower(c))) ? "an" : "a";
 			return "Give me " + article + ".. " + c;
+		}
+			
+		private static string BDayLine (string DOB)
+		{
+			DateTime bday = makeBdayDateTime (DOB);
+			int difference =  bday.DayOfYear - DateTime.Today.DayOfYear;
+			string days;
+			if (difference > 0) {
+				days = difference.ToString ();
+			} else if (difference < 0) {
+				days = (365 + difference).ToString ();
+			} else {
+				days = "Something has gone horribly wrong.";
+			}
+			return "Your birthday is " + days + " days away!";
+		}
+
+		private static bool isBirthday (DateTime bday)
+		{
+			if (bday.DayOfYear == DateTime.Today.DayOfYear)
+				return true;
+			return false;
+		}
+
+		private static DateTime makeBdayDateTime (string DOB)
+		{
+			string[] month_day = DOB.Split ('/');
+			DateTime bday = new DateTime(DateTime.Today.Year, int.Parse(month_day [0]), int.Parse(month_day [1]));
+			return bday;
 		}
 	}
 }
